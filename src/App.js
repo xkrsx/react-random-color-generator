51dcc05331c053aa22ad0164d04501b3fdcc0535
Jakub Markiewicz <markiewicz267@gmail.com>
2024-05-16 14:43:18 +0200

    Update App.js
    Fix problems

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ export default function App() {
   ) {
     randomcolor({ luminosity: luminosity });
   } else {
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    randomColor;
+    randomcolor();
   }
 
   return (
